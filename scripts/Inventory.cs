91c6c621072c64bcabc6f50ae48933a27d92ebe8
IneedtosubmitmyNEA <maaheemr@gmail.com>
2025-03-20 05:27:25 +0000

    Update Inventory.cs

--- scripts/Inventory.cs
+++ scripts/Inventory.cs
@@ -39,7 +39,7 @@ public class Inventory : MonoBehaviour
         if(canOpen)
         {
             RevealInventory();
-        }                           ///checks if the player can open inventory or not
+        }                           ///checks if the player can open inventory UIs or not
         else
             HideInventory();
         changeEquippedOnHUD(inventory[whereIsMain]);// code to make add the main item to the hotbar
@@ -47,7 +47,7 @@ public class Inventory : MonoBehaviour
         death();
 
     }
-    void canOpenInventory()
+    void canOpenInventory()// check if the player can open their inventory
     {
         if(canOpen && Input.GetKeyUp(OpenInventory) && !levelAreaScript.isLeveling && !isDeath)
         {
@@ -61,27 +61,27 @@ public class Inventory : MonoBehaviour
 
     void RevealInventory()
     {
-        playerInventory.SetActive(true);
+        playerInventory.SetActive(true);//shows invetory UI
         PlayerHud.SetActive(false);
         Cursor.lockState = CursorLockMode.None;
         Cursor.visible = true;
     }
 
-    void HideInventory()
+    void HideInventory()//shows playerHUD UI
     {
         playerInventory.SetActive(false);
         PlayerHud.SetActive(true);
-        if (!levelAreaScript.isLeveling)
+        if (!levelAreaScript.isLeveling)// if the player is leveling they have the HUD as the background so this allows them to use their mouse
         {Cursor.lockState = CursorLockMode.Locked;
         Cursor.visible = false;}
     }
     //to get the weapon that the player has as their main weapon
     
-    public void AddToInventory(InventoryItemData item, TMP_Text text, GameObject SwapToMainButton, GameObject UseItemButton, Inventory inv)
+    public void AddToInventory(InventoryItemData item, TMP_Text text, GameObject SwapToMainButton, GameObject UseItemButton, Inventory inv)//this code makes it so that the item in the inventory list is shown in the UI
     {
         if(inventory != null)
         {
-            for (int i = 0; i < inventorySlots.Length; i++)
+            for (int i = 0; i < inventorySlots.Length; i++)//code to look for children, if theres a child goes to the next item slot, until an item slot with no child is found
             {
                 InventorySlot slot = inventorySlots[i];
                 InventoryItem itemInSlot = slot.GetComponentInChildren<InventoryItem>();
@@ -95,7 +95,7 @@ public class Inventory : MonoBehaviour
         }
     }
 
-    void SpawnNewItem(InventoryItemData item, InventorySlot slot, TMP_Text text, GameObject SwapToMainButton, GameObject UseItemButton, Inventory inv)
+    void SpawnNewItem(InventoryItemData item, InventorySlot slot, TMP_Text text, GameObject SwapToMainButton, GameObject UseItemButton, Inventory inv)// places the item as a child of the free slot and them and then changes the variables /assigns variables
     {
         GameObject newItemGo = Instantiate(inventoryItemPrefab, slot.transform);
         InventoryItem inventoryItem = newItemGo.GetComponent<InventoryItem>();
@@ -106,7 +106,7 @@ public class Inventory : MonoBehaviour
         newItemGo.GetComponent<InventoryItem>().Inv=inv;
     }
 
-    void EquippedOnHud(InventoryItemData item)
+    void EquippedOnHud(InventoryItemData item)// similar to the method above except its for a specific index of a different list
     {
         GameObject newItemGo = Instantiate(inventoryItemPrefab, mainSlots[0].transform);
         InventoryItem inventoryItem = newItemGo.GetComponent<InventoryItem>();
@@ -121,12 +121,12 @@ public class Inventory : MonoBehaviour
             EquippedOnHud(item);
             shouldchange=false;
         }
-        if(inventory != null && mainSlots[0].GetComponentInChildren<InventoryItem>() == null && item.isMain)
+        if(inventory != null && mainSlots[0].GetComponentInChildren<InventoryItem>() == null && item.isMain)//if the first is the main weapon then this triggers
         {
             UnityEngine.Debug.Log("long if statement has been triggered triggered");
             EquippedOnHud(item);
         }
-        else if(inventory !=null && !item.isMain)
+        else if(inventory !=null && !item.isMain)//looks for the main item
         {//searches the list to find what has become the main weapon
             for (int i = 0; i < inventory.Count; i++)
             {
@@ -145,13 +145,13 @@ public class Inventory : MonoBehaviour
         
     }
 
-    public void useHealthItem()
+    public void useHealthItem()// lets the player consume their health item with a keybind
     {
         if(Input.GetKeyDown(UseHealthItemKey) && inventory[1].amount>0 && playerStats.health<playerStats.healthMAX)
         {    
             inventory[1].amount-=1;
             playerStats.health+=inventory[1].health;
-            text.text = $"{inventory[1].displayName}\nHealth restored: {inventory[1].health}\nAmount: {inventory[1].amount}";
+            text.text = $"{inventory[1].displayName}\nHealth restored: {inventory[1].health}\nAmount: {inventory[1].amount}";// updates the text bottom right of the inventory
             HealthItemRemainingText.text = $"{inventory[1].amount}";
             if (playerStats.health>playerStats.healthMAX)//makes sure that the players maximum health does not get exceeded
             {playerStats.health=playerStats.healthMAX;}
@@ -163,23 +163,23 @@ public class Inventory : MonoBehaviour
     {
         if (playerStats.health<=0)
         {
-            isDeath=true;
-            deathscreen.SetActive(true);
-            inventory[2].amount+=1;
-            canOpen=false;
-            playerStats.health=0.001f;
-            Invoke("respawn",2.5f);
+            isDeath=true;//in movement code, makes the player unable to move
+            deathscreen.SetActive(true);shows the death screen
+            inventory[2].amount+=1;// increase the death item amount
+            canOpen=false;// does not let the player open the inventory
+            playerStats.health=0.001f;// makes sure that this code does not loop but also mkaes the health bar look like its fully gone
+            Invoke("respawn",2.5f);// respawn the player after 2.5 seconds
         }
     }
 
     public void respawn()
     {
         isDeath=false;
-        deathscreen.SetActive(false);
+        deathscreen.SetActive(false);//hides the death screen
         playerStats.health=playerStats.healthMAX;
-        playerStats.stamina=playerStats.staminaMax;
-        inventory[1].amount=5;
-        transform.position=playerStats.respawn;
+        playerStats.stamina=playerStats.staminaMax;// make the player regain their maximum health and stamina
+        inventory[1].amount=5;// resets their health item amount
+        transform.position=playerStats.respawn;// makes the player telaport to the levelUpArea
     }
 
 }
